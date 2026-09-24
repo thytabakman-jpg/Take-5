5089cb94243cea6c637878fb1d2608267931ebd7
thytabakman-jpg <thytabakman@gmail.com>
2026-09-24 19:34:50 -0400

    Upgrade mode selector to three-axis mode cube

--- runtime/mode_selector.py
+++ runtime/mode_selector.py
@@ -1,19 +1,12 @@
-"""Experimental four-quadrant projection.
-
-This is a research projection, not a settled controller ontology.
-It must not be treated as authoritative mode geometry until competing
-operational-mode models survive recursive/external challenge and holdouts.
-"""
-MODEL_STATUS="EXPERIMENTAL_PROJECTION"
-
-def model_status():
-    return MODEL_STATUS
-
-def select_mode(*,premature_pruning=False,exact_discriminant=False,bounded_intervention=False,broad_redesign=False):
-    flags=[premature_pruning,exact_discriminant,bounded_intervention,broad_redesign]
-    if sum(bool(x) for x in flags)!=1:
+"""Mode-cube selector: breadth x direction x coupling."""
+def select_mode(*,premature_pruning=False,exact_discriminant=False,bounded_intervention=False,broad_redesign=False,
+                independent_local=False,coupled=False):
+    base=[premature_pruning,exact_discriminant,bounded_intervention,broad_redesign]
+    if sum(bool(x) for x in base)!=1 or (independent_local and coupled):
         return "OPEN"
-    if premature_pruning: return "EXPAND_OBSERVE"
-    if exact_discriminant: return "CONTRACT_OBSERVE"
-    if bounded_intervention: return "CONTRACT_ACT"
-    return "EXPAND_ACT"
+    if premature_pruning: bd="EXPAND_OBSERVE"
+    elif exact_discriminant: bd="CONTRACT_OBSERVE"
+    elif bounded_intervention: bd="CONTRACT_ACT"
+    else: bd="EXPAND_ACT"
+    cp="DECOUPLED" if independent_local else "COUPLED"
+    return bd+"_"+cp
