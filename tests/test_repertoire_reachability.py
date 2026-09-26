6cd79e8702645cf23b3d0887c787ae47d06dd573
thytabakman-jpg <thytabakman@gmail.com>
2026-09-26 03:32:11 -0400

    Test current repertoire reachability

---
+++ tests/test_repertoire_reachability.py
@@ -0,0 +1,9 @@
+from repertoire_reachability import audit_current_repertoire_reachability
+from tool_run_registry import CONFIGURED_RUNS
+
+def test_current_finite_repertoire_has_controller_execution_reachability_evidence():
+    out=audit_current_repertoire_reachability()
+    assert out.status=="CLOSED_RELATIVE"
+    assert out.failures==()
+    assert out.identity_checked==len(CONFIGURED_RUNS)
+    assert out.transition_checked==len(CONFIGURED_RUNS)
